91a40ddbd7599703c34ecb6caabdb8b3e88a950b
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2025-10-06 20:11:38 -0600

    Add files via upload

---
+++ EVA2_9_FOR/src/eva2_9_for/EVA2_9_FOR.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_for;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA2_9_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametros para funcionar
+        //punto inicial
+        //donde acaba
+        //como me voy a mover
+        //i--->indice (arreglo)
+        //++ sumar uno,-- restar uno
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i + "hola mundo");
+        }
+             for(int i = 1; i <= 10; i++){
+            System.out.println(i + "hola mundo");
+            
+            
+        }
+    }
+    
+}
